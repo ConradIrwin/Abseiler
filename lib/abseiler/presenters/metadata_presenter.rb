69ce5e3af64de47be21d6c1871c54dbdcbafdcdd
Sam Elliott <sam@lenary.co.uk>
2011-09-11 17:00:06 +0100

    metadata presenter

--- lib/abseiler/presenters/metadata_presenter.rb
+++ lib/abseiler/presenters/metadata_presenter.rb
@@ -1,10 +1,36 @@
 module Abseiler
   class MetadataPresenter
     def initialize(metadata)
+      @metadata = metadata
+      @templater = Templater.new(metadata[:short_name])
+    end
+
+    def to_hash
+      hash = default_hash.merge(@metadata)
+
+      hash
     end
 
     def to_json
-      "{}"
+      to_hash.to_json
+    end
+
+    private
+
+    def default_hash
+      {
+        description:    "",
+        welcome_text:   "",
+        icon_url:       "https://abseiler.heroku.com/a/#{@metadata[:short_name]}/icon.png",
+        small_icon_url: "https://abseiler.heroku.com/a/#{@metadata[:short_name]}/small_icon.png",
+        preview_url:    "https://abseiler.heroku.com/a/#{@metadata[:short_name]}/preview.png",
+        provider_name:  "Lenary",
+        provider_url:   "http://lenary.co.uk/",
+        provider: {
+          name: "Lenary",
+          url:  "http://lenary.co.uk/",
+        },
+      }
     end
   end
 end
--- test/presenters/metadata_presenter_test.rb
+++ test/presenters/metadata_presenter_test.rb
@@ -2,4 +2,25 @@ require File.expand_path(File.dirname(__FILE__)) + '/../test_helper.rb'
 
 describe Abseiler::MetadataPresenter do
   include Abseiler
+
+  before do
+    @presenter = MetadataPresenter.new({short_name: "test"})
+  end
+
+  it "should have a description" do
+    @presenter.to_hash.must_be :has_key?, :description
+
+    other_presenter = MetadataPresenter.new({short_name: "test", description: "Foo Bar"})
+    other_presenter.to_hash[:description].must_equal "Foo Bar"
+  end
+
+  it "should come out as json" do
+    @presenter.to_hash.must_be_instance_of Hash
+    JSON.parse(@presenter.to_json).must_be_instance_of Hash
+  end
+
+  it "should render the welcome template if it exists" do
+    @presenter.to_hash.must_be :has_key?, :welcome_text
+    @presenter.to_hash[:welcome_text].must_match /Testy/
+  end
 end
